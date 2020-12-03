2398ded5d3f2c78827b5794994eb8e7415a0a1c0
kanhaa <75423572+kanhaa@users.noreply.github.com>
2020-12-03 19:43:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "1431637832:AAH2LQ3Ur6ssxtLR1f2i-4Kfts4rGWxidgQ"
 
 def download(bot, update):
     message = update.effective_message
